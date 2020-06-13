9cf9ffb675f65ed908a544ef00f0cbb6f2bc5842
Kenneth Arnesen <19003250@vcconnect.co.za>
2020-06-13 07:54:01 +0200

    Added Attributes/FK's to JobcardMaterial table

--- DomingoRoofWorks/MetaDomingoDatabase/dbo/Tables/JobcardMaterial.sql
+++ DomingoRoofWorks/MetaDomingoDatabase/dbo/Tables/JobcardMaterial.sql
@@ -1,4 +1,13 @@
 ﻿CREATE TABLE [dbo].[JobcardMaterial]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[JobcardMaterialId]   NVARCHAR (450)  NOT NULL,
+    [JobcardId]           NVARCHAR (450)  NOT NULL,
+    [CreatedAt]           DATETIME2 (7)   NOT NULL,
+    [ItemId]              NVARCHAR (450)  NOT NULL,
+    [Quantity]            INT            NOT NULL,
+    [TaxId]               NVARCHAR (450)  NOT NULL,
+    CONSTRAINT [PK_JobcardMaterialId] PRIMARY KEY CLUSTERED ([JobcardMaterialId] ASC),
+    CONSTRAINT [FK_JobcardMaterial_Item_ItemId] FOREIGN KEY ([ItemId]) REFERENCES [dbo].[Item] ([ItemId]),
+    CONSTRAINT [FK_JobcardMaterial_Jobcard_JobcardId] FOREIGN KEY ([JobcardId]) REFERENCES [dbo].[Jobcard] ([JobcardId]),
+    CONSTRAINT [FK_JobcardMaterial_Tax_TaxId] FOREIGN KEY ([TaxId]) REFERENCES [dbo].[Tax] ([TaxId])
 )
