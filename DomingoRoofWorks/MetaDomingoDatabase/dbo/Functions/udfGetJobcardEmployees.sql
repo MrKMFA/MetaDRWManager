2d29cd1bb99047331e386e236222e4279a3d73f5
Kenneth Arnesen <19003250@vcconnect.co.za>
2020-06-13 12:03:05 +0200

    Created Table-Valued Function udfGetJobcardEmployees

---
+++ DomingoRoofWorks/MetaDomingoDatabase/dbo/Functions/udfGetJobcardEmployees.sql
@@ -0,0 +1,38 @@
+﻿CREATE FUNCTION [dbo].[udfGetJobcardEmployees]
+(
+	@JobcardId nvarchar(450)
+)
+RETURNS @retEmployeesAssigned TABLE
+(
+	EmployeeId nvarchar(450),
+	CreatedBy nvarchar(450),
+	CreatedAt datetime2(7),
+	AssignedStartDate datetime2(7),
+	AssignedEndDate datetime2(7),
+	EmployeeFName nvarchar(max),
+	EmployeeSName nvarchar(max),
+	Phone nvarchar(max)
+)
+AS
+BEGIN
+	IF @JobcardId IS NOT NULL
+		BEGIN
+		IF EXISTS(SELECT * FROM [EmployeeJobcard] x
+					WHERE x.JobcardId = @JobcardId)
+			INSERT INTO  @retEmployeesAssigned
+				SELECT
+					x.EmployeeId,
+					x.CreatedBy,
+					x.CreatedAt,
+					x.AssignedStartDate,
+					x.AssignedEndDate,
+					e.EmployeeFName,
+					e.EmployeeSName,
+					e.Phone
+				FROM [Employee] AS e
+					INNER JOIN [EmployeeJobcard] x
+					ON e.EmployeeId = x.EmployeeId
+				WHERE x.JobcardId = @JobcardId
+		END
+	RETURN;
+END;
