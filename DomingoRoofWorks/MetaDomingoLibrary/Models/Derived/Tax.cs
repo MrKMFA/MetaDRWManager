d33e0d4d5613b00fbcba2276c4b035ff9aebcefa
Kenneth Arnesen <19003250@vcconnect.co.za>
2020-06-19 23:24:32 +0200

    Updated Tax Class, Added Constructors

--- DomingoRoofWorks/MetaDomingoLibrary/Models/Derived/Tax.cs
+++ DomingoRoofWorks/MetaDomingoLibrary/Models/Derived/Tax.cs
@@ -1,7 +1,7 @@
 ﻿//Author: Kenneth Arnesen
 //Date Created: 2020/06/16
 //Description:
-//Last Updated: 2020/06/17
+//Last Updated: 2020/06/19
 
 using MetaDomingoLibrary.Models.Base;
 using System;
@@ -10,12 +10,15 @@ namespace MetaDomingoLibrary.Models.Derived
 {
     public class Tax : LookUp
     {
-        // Private Fields
+        // *** Private Fields ***
         private string taxId;
         private string taxLabel;
         private decimal taxPerc;
 
-        // Constructors
+
+
+        // *** Constructors ***
+        //-Used when instantiating default object and included base class
         public Tax() : base()
         {
             taxId = "TAX" + DateTime.UtcNow.Date.Year.ToString() +
@@ -23,7 +26,30 @@ namespace MetaDomingoLibrary.Models.Derived
                 DateTime.UtcNow.Date.Day.ToString() + Guid.NewGuid().ToString().Substring(0, 4).ToUpper();
         }
 
-        // Properties
+        //-Used when instantiating default object (and base class) with initializing property values
+        public Tax(string txLabel, decimal txPerc)
+            : base()
+        {
+            taxId = "TAX" + DateTime.UtcNow.Date.Year.ToString() +
+                DateTime.UtcNow.Date.Month.ToString() +
+                DateTime.UtcNow.Date.Day.ToString() + Guid.NewGuid().ToString().Substring(0, 4).ToUpper();
+            taxLabel = txLabel;
+            taxPerc = txPerc;
+        }
+
+        //-Used when instantiating objects and initializing with values retrieved from database
+        public Tax(string tId, string txLabel, decimal txPerc,
+                    DateTime createdAt, DateTime modified)
+            : base(createdAt, modified)
+        {
+            taxId = tId;
+            taxLabel = txLabel;
+            taxPerc = txPerc;
+        }
+
+
+
+        // *** Properties ***
         public string TaxId
         {
             get
