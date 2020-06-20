a871296d736e17ae6b83c0f3ca36bb953e1f2732
Kenneth Arnesen <19003250@vcconnect.co.za>
2020-06-20 06:10:13 +0200

    Deleted New Folder

--- DomingoRoofWorks/MetaDomingoDatabase/dbo/Tables/New/Person.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Person]
-(
-	[PersonId]                  NVARCHAR(14)    PRIMARY KEY NOT NULL,
-	[EntityId]                  NVARCHAR(14)	NOT NULL,
-	[FirstName]                 NVARCHAR(14)    ,
-	[LastName]                  NVARCHAR(14)    ,
-	[CreatedAt]                 DATETIME2(7)    NOT NULL,
-	[ModifiedDate]              DATETIME2(7)    NOT NULL,
-)
