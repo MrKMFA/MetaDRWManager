eeeab1b0f945d6809d617298f46fa6ea2722afe0
Kenneth Arnesen <19003250@vcconnect.co.za>
2020-06-20 06:26:53 +0200

    Created Jobtype Table

---
+++ DomingoRoofWorks/MetaDomingoDatabase/dbo/Tables/Jobtype.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Jobtype]
+(
+	[JobtypeId]				NVARCHAR(14)    PRIMARY KEY NOT NULL,
+	[JobtypeName]			NVARCHAR(14)    ,
+	[JobtypeDescription]    NVARCHAR(14)    ,
+	[Rate]					MONEY			,
+	[CreatedAt]				DATETIME2(7)    NOT NULL,
+    [ModifiedDate]			DATETIME2(7)    NOT NULL
+)
