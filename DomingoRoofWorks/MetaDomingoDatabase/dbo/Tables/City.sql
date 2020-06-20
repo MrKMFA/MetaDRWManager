1277c2fc2e577ee6036b975d5c03fa0c2e0e08e1
Kenneth Arnesen <19003250@vcconnect.co.za>
2020-06-20 06:10:51 +0200

    Moved New Table Definitions

---
+++ DomingoRoofWorks/MetaDomingoDatabase/dbo/Tables/City.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[City]
+(
+	[CityId]          NVARCHAR(14)    PRIMARY KEY NOT NULL,
+	[CityCode]        NVARCHAR(14)    ,
+	[CityName]        NVARCHAR(14)    ,
+	[CreatedAt]       DATETIME2(7)    NOT NULL,
+    [ModifiedDate]    DATETIME2(7)    NOT NULL
+)
---
+++ DomingoRoofWorks/MetaDomingoDatabase/dbo/Tables/Person.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Person]
+(
+	[PersonId]                  NVARCHAR(14)    PRIMARY KEY NOT NULL,
+	[EntityId]                  NVARCHAR(14)	NOT NULL,
+	[FirstName]                 NVARCHAR(14)    ,
+	[LastName]                  NVARCHAR(14)    ,
+	[CreatedAt]                 DATETIME2(7)    NOT NULL,
+	[ModifiedDate]              DATETIME2(7)    NOT NULL,
+)
