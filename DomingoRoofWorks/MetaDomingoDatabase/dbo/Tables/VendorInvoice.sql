3f51714d85dded40d3b109daa2bcaf8412dec327
Kenneth Arnesen <19003250@vcconnect.co.za>
2020-06-20 06:56:16 +0200

    Created VendorInvoice Table

---
+++ DomingoRoofWorks/MetaDomingoDatabase/dbo/Tables/VendorInvoice.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[VendorInvoice]
+(
+	[VendorInvoiceId]		NVARCHAR(14)    PRIMARY KEY NOT NULL,
+	[VendorId]				NVARCHAR(14)	NOT NULL,
+	[InvoiceId]				NVARCHAR(14)	NOT NULL,
+	[CreatedAt]             DATETIME2(7)    NOT NULL,
+    [ModifiedDate]          DATETIME2(7)    NOT NULL
+)
