bad6c46234731a15160d7e7a6410bc18153f51d8
Kenneth Arnesen <19003250@vcconnect.co.za>
2020-06-20 06:22:25 +0200

    Created Tax Table

---
+++ DomingoRoofWorks/MetaDomingoDatabase/dbo/Tables/Tax.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Tax]
+(
+	[TaxId]           NVARCHAR(14)    PRIMARY KEY NOT NULL,
+	[TaxLabel]        NVARCHAR(14)    ,
+	[TaxPerc]		  DECIMAL(3,2)	  ,
+	[CreatedAt]       DATETIME2(7)    NOT NULL,
+    [ModifiedDate]    DATETIME2(7)    NOT NULL
+)
