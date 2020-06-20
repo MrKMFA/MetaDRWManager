4d8637b6ef6cd0cc92531fdad960b204b50ebf14
Kenneth Arnesen <19003250@vcconnect.co.za>
2020-06-20 03:30:37 +0200

    Added InvoiceItem Class with Constructors and Properties

--- DomingoRoofWorks/MetaDomingoConsole/Program.cs
+++ DomingoRoofWorks/MetaDomingoConsole/Program.cs
@@ -9,14 +9,16 @@ namespace MetaDomingoConsole
         {
             Customer kenneth = new Customer();
 
-            Console.WriteLine(kenneth.CustomerId);
-            Console.WriteLine(kenneth.PersonId);
-            Console.WriteLine(kenneth.EntityId);
-            Console.WriteLine(kenneth.CreatedAt);
+            //Console.WriteLine(kenneth.CustomerId);
+            //Console.WriteLine(kenneth.PersonId);
+            //Console.WriteLine(kenneth.EntityId);
+            //Console.WriteLine(kenneth.CreatedAt);
 
             CustomerInvoice obj = new CustomerInvoice(kenneth);
             Console.WriteLine(obj.CustomerInvoiceId);
             Console.WriteLine(obj.InvoiceId);
+            Console.WriteLine(obj.Customer.EntityId);
+            Console.WriteLine(obj.Customer.PersonId);
             Console.WriteLine(obj.Customer.CustomerId);
             Console.WriteLine(obj.CreatedAt);
             Console.WriteLine(obj.ModifiedDate);
---
+++ DomingoRoofWorks/MetaDomingoLibrary/Models/Derived/InvoiceItem.cs
@@ -0,0 +1,110 @@
+﻿//Author: Kenneth Arnesen
+//Date Created: 2020/06/20
+//Description:
+//Last Updated: 2020/06/20
+
+using MetaDomingoLibrary.Models.Base;
+using System;
+
+namespace MetaDomingoLibrary.Models.Derived
+{
+    public class InvoiceItem : LookUp
+    {
+        // *** Private Fields ***
+        private string invoiceItemId;
+        private string invoiceId;
+        private string itemId;
+        private Item item;
+        private int quantity;
+
+
+
+        // *** Constructors ***
+        //-Used when instantiating default object and included base class
+        //-Used when instantiating default object (and base class) with initializing property values
+        public InvoiceItem(string invId, Item item, int qty) : base()
+        {
+            invoiceItemId = "IVI" + DateTime.UtcNow.Date.Year.ToString() +
+                DateTime.UtcNow.Date.Month.ToString() +
+                DateTime.UtcNow.Date.Day.ToString() + Guid.NewGuid().ToString().Substring(0, 4).ToUpper();
+            this.invoiceId = invId;
+            this.item = item;
+            this.quantity = qty;
+        }
+
+        //-Used when initializing objects with values retrieved from database
+        public InvoiceItem(string invItemId, string invId, string itemId,
+                            int qty, DateTime created, DateTime modified,
+                            Item item = null)
+            : base(created, modified)
+        {
+            this.invoiceItemId = invItemId;
+            this.invoiceId = invId;
+            this.itemId = itemId;
+            this.item = item;
+            this.quantity = qty;
+        }
+
+
+
+        // *** Properties ***
+        public string InvoiceItemId
+        {
+            get
+            {
+                return this.InvoiceItemId;
+            }
+        }
+
+        public string InvoiceId
+        {
+            get
+            {
+                return this.invoiceId;
+            }
+            set
+            {
+                this.invoiceId = value;
+            }
+        }
+
+        public string ItemId
+        {
+            get
+            {
+                return this.itemId;
+            }
+
+            set
+            {
+                this.itemId = value;
+            }
+        }
+
+        public Item Item
+        {
+            get
+            {
+                return this.item;
+            }
+            set
+            {
+                this.item = value;
+                base.ModifiedDate = DateTime.UtcNow;
+            }
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return this.quantity;
+            }
+            set
+            {
+                this.quantity = value;
+                base.ModifiedDate = DateTime.UtcNow;
+            }
+        }
+    }
+}
