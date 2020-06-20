168252df0eec32396ee719960a04c844a08e0f17
Kenneth Arnesen <19003250@vcconnect.co.za>
2020-06-20 06:15:14 +0200

    Added Currency Table

---
+++ DomingoRoofWorks/MetaDomingoDatabase/dbo/Tables/Currency.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Currency]
+(
+	[CurrencyId]      NVARCHAR(14)    PRIMARY KEY NOT NULL,
+	[CurrencySymbol]  NVARCHAR(14)    ,
+	[CurrencyCode]    NVARCHAR(14)    ,
+	[CurrencyName]    NVARCHAR(14)    ,
+	[CreatedAt]       DATETIME2(7)    NOT NULL,
+    [ModifiedDate]    DATETIME2(7)    NOT NULL
+)
