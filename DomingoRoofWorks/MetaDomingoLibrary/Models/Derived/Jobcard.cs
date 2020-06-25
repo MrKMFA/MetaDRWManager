87a5e68cce17fe9bb63409f2730237cf9cfdec31
Kenneth Arnesen <19003250@vcconnect.co.za>
2020-06-25 04:23:09 +0200

    Updated Jobcard, Added Constructors

--- DomingoRoofWorks/MetaDomingoLibrary/Models/Derived/Jobcard.cs
+++ DomingoRoofWorks/MetaDomingoLibrary/Models/Derived/Jobcard.cs
@@ -23,11 +23,100 @@ namespace MetaDomingoLibrary.Models.Derived
 
         // *** Constructors ***
         //-Used when instantiating default object and included base class
+        public Jobcard() : base()
+        {
+            this.jobcardId = "JCD" + DateTime.UtcNow.Date.Year.ToString() +
+                DateTime.UtcNow.Date.Month.ToString() +
+                DateTime.UtcNow.Date.Day.ToString() + Guid.NewGuid().ToString().Substring(0, 4).ToUpper();
+
+            this.materials = new List<JobcardItem>();
+            this.employeesAssigned = new List<JobcardEmployee>();
+        }
 
         //-Used when instantiating default object (and base class) with initializing property values
+        public Jobcard(Employee createdBy, Customer client)
+            : base()
+        {
+            this.jobcardId = "JCD" + DateTime.UtcNow.Date.Year.ToString() +
+                DateTime.UtcNow.Date.Month.ToString() +
+                DateTime.UtcNow.Date.Day.ToString() + Guid.NewGuid().ToString().Substring(0, 4).ToUpper();
+
+            this.createdBy = createdBy;
+            this.client = client;
+
+            this.materials = new List<JobcardItem>();
+            this.employeesAssigned = new List<JobcardEmployee>();
+        }
+
+        public Jobcard(Employee createdBy, Customer client, Jobtype jobtype)
+            : base()
+        {
+            this.jobcardId = "JCD" + DateTime.UtcNow.Date.Year.ToString() +
+                DateTime.UtcNow.Date.Month.ToString() +
+                DateTime.UtcNow.Date.Day.ToString() + Guid.NewGuid().ToString().Substring(0, 4).ToUpper();
+
+            this.createdBy = createdBy;
+            this.client = client;
+            this.jobtype = jobtype;
+
+            this.materials = new List<JobcardItem>();
+            this.employeesAssigned = new List<JobcardEmployee>();
+        }
+
+        public Jobcard(Employee createdBy, Customer client, Jobtype jobtype,
+                        DateTime jobStart, DateTime jobEnd) 
+            : base()
+        {
+            this.jobcardId = "JCD" + DateTime.UtcNow.Date.Year.ToString() +
+                DateTime.UtcNow.Date.Month.ToString() +
+                DateTime.UtcNow.Date.Day.ToString() + Guid.NewGuid().ToString().Substring(0, 4).ToUpper();
+
+            this.createdBy = createdBy;
+            this.client = client;
+            this.jobtype = jobtype;
+            this.jobStartDate = jobStart;
+            this.jobEndDate = jobEnd;
+
+            this.materials = new List<JobcardItem>();
+            this.employeesAssigned = new List<JobcardEmployee>();
+        }
+
+        public Jobcard(Employee createdBy, Customer client, Jobtype jobtype,
+                        DateTime jobStart, DateTime jobEnd, List<JobcardItem> materials,
+                        List<JobcardEmployee> employeesAssigned)
+            : base()
+        {
+            this.jobcardId = "JCD" + DateTime.UtcNow.Date.Year.ToString() +
+                DateTime.UtcNow.Date.Month.ToString() +
+                DateTime.UtcNow.Date.Day.ToString() + Guid.NewGuid().ToString().Substring(0, 4).ToUpper();
+
+            this.createdBy = createdBy;
+            this.client = client;
+            this.jobtype = jobtype;
+            this.jobStartDate = jobStart;
+            this.jobEndDate = jobEnd;
+
+            this.materials = materials;
+            this.employeesAssigned = employeesAssigned;
+        }
 
         //-Used when initializing objects with values retrieved from database
+        public Jobcard(string jobcardId, Employee createdBy, Customer client, Jobtype jobtype,
+                        DateTime jobStart, DateTime jobEnd, List<JobcardItem> materials,
+                        List<JobcardEmployee> employeesAssigned, DateTime createdAt, DateTime modifiedAt)
+            : base(createdAt, modifiedAt)
+        {
+            this.jobcardId = jobcardId;
 
+            this.createdBy = createdBy;
+            this.client = client;
+            this.jobtype = jobtype;
+            this.jobStartDate = jobStart;
+            this.jobEndDate = jobEnd;
+
+            this.materials = materials;
+            this.employeesAssigned = employeesAssigned;
+        }
 
         // *** Properties ***
     }
