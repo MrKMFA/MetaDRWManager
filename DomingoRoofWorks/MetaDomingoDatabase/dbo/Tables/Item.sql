f5839ffb1adeffcaf4c5539081a16dbbd8368433
Kenneth Arnesen <19003250@vcconnect.co.za>
2020-06-25 07:30:43 +0200

    Created Item Table with Foreign Key References

---
+++ DomingoRoofWorks/MetaDomingoDatabase/dbo/Tables/Item.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Item]
+(
+	[ItemId]			NVARCHAR(14)	PRIMARY KEY NOT NULL,
+	[ItemName]			NVARCHAR(14)	NOT NULL,
+	[ItemDescription]	NVARCHAR(14)	NOT NULL,
+	[VendorId]			NVARCHAR(14)	NOT NULL,
+	[PreferredVendor]	BIT				NOT NULL,
+	[CostPrice]			MONEY			NOT NULL,
+	[MarkupPerc]		DECIMAL(3,2)	NOT NULL,
+	[UnitPrice] AS CostPrice * MarkupPerc PERSISTED,
+	[CreatedAt]			DATETIME2(2)	NOT NULL,
+	[ModifiedDate]		DATETIME2(2)	NOT NULL,
+	CONSTRAINT [FK_Item_ToVendor] FOREIGN KEY (VendorId) REFERENCES Vendor(VendorId)
+)
