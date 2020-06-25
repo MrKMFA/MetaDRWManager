cb3e94baed5a4a9e30b7986727fbc5aa4eb3c991
Kenneth Arnesen <19003250@vcconnect.co.za>
2020-06-25 07:07:35 +0200

    Updated Jobcard, Added Lookup Attributes

--- DomingoRoofWorks/MetaDomingoDatabase/dbo/Tables/Jobcard.sql
+++ DomingoRoofWorks/MetaDomingoDatabase/dbo/Tables/Jobcard.sql
@@ -6,6 +6,8 @@
 	[JobtypeId]		NVARCHAR(14)	NOT NULL,
 	[JobStartDate]	DATETIME2(7)	NOT NULL,
 	[JobEndDate]	DATETIME2(7)	NOT NULL,
+	[CreatedAt]		DATETIME2(7)	NOT NULL,
+	[ModifiedDate]	DATETIME2(7)	NOT NULL,
 	CONSTRAINT [FK_Jobcard_ToEmployee] FOREIGN KEY (CreatedBy) REFERENCES Employee(EmployeeId),
 	CONSTRAINT [FK_Jobcard_ToCustomer] FOREIGN KEY (CustomerId) REFERENCES Customer(CustomerId),
 	CONSTRAINT [FK_Jobcard_ToJobtype] FOREIGN KEY (JobtypeId) REFERENCES Jobtype(JobtypeId)
