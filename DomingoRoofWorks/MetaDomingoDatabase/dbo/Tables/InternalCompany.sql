59a6f5a844a8a68c3c48e2323da5f6be96368411
Kenneth Arnesen <19003250@vcconnect.co.za>
2020-06-25 06:50:43 +0200

    Added InternalCompany Table

---
+++ DomingoRoofWorks/MetaDomingoDatabase/dbo/Tables/InternalCompany.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[InternalCompany]
+(
+	[InternalCompanyId]		NVARCHAR(14)    PRIMARY KEY NOT NULL,
+	[EntityId]				NVARCHAR(14)    NOT NULL,
+	[InternalComanyName]	NVARCHAR(50)    NOT NULL,
+	CONSTRAINT [FK_InternalCompany_ToBusinessEntity] FOREIGN KEY (EntityId) REFERENCES BusinessEntity(EntityId)
+)
