769ca88e32c19e4709c44970df2c9cc4bd3f14d1
Kenneth Arnesen <19003250@vcconnect.co.za>
2020-06-25 07:31:00 +0200

    Created JobcardEmployee Table

---
+++ DomingoRoofWorks/MetaDomingoDatabase/dbo/Tables/JobcardEmployee.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[JobcardEmployee]
+(
+	[JobcardEmployeeId]		NVARCHAR(14)	PRIMARY KEY NOT NULL,
+	[JobcardId]				NVARCHAR(14)	NOT NULL,
+	[EmployeeId]			NVARCHAR(14)	NOT NULL,
+	[AssignedStartDate]		DATETIME2(2)	NOT NULL,
+	[AssignedEndDate]		DATETIME2(2)	NOT NULL,
+	[CreatedAt]				DATETIME2(2)	NOT NULL,
+	[ModifiedDate]			DATETIME2(2)	NOT NULL,
+	CONSTRAINT [FK_JobcardEmployee_ToJobcard] FOREIGN KEY (JobcardId) REFERENCES Jobcard(JobcardId),
+	CONSTRAINT [FK_JobcardEmployee_ToEmployee] FOREIGN KEY (EmployeeId) REFERENCES Employee(EmployeeId)
+)
